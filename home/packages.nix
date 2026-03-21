5dee10d80dcf70fbb22b5510c38f8b66ab5ebce7
tilloest <178488902+tilloest@users.noreply.github.com>
2026-03-21 20:31:45 +0200

    refactored home and environment packages

--- home/packages.nix
+++ home/packages.nix
@@ -4,5 +4,17 @@
     home.packages = with pkgs; [
         obsidian
         bibata-cursors
+        onlyoffice-desktopeditors
+        floorp-bin
+        discord
+        qalculate-qt
+        pinta
+        parabolic
+        inkscape
+        localsend
+        heroic
+        czkawka
+        tealdeer
+        fastfetch
     ];
 }
--- modules/programs/packages.nix
+++ modules/programs/packages.nix
@@ -2,34 +2,22 @@
 
 {
   environment.systemPackages = with pkgs; [
-    onlyoffice-desktopeditors
-    tealdeer
     wget
     git
-    mangohud
-    floorp-bin
-    fastfetch
-    discord
-    easyeffects
-    lsp-plugins
     unrar
     unzip
-    ghostty
+    easyeffects
+    lsp-plugins
+    mangohud
     winetricks
     protontricks
-    qalculate-qt
     protonplus
-    pinta
-    parabolic
-    inkscape
-    input-remapper
-    localsend
-    heroic
-    czkawka
     vulkan-tools
     vulkan-loader
     vulkan-headers
     lsfg-vk
     lsfg-vk-ui
+    ghostty
+    input-remapper
   ];
 }
